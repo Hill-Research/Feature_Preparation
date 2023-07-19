66e518711134c45511644a11b05d5703f4a2d535
rli92 <rui.li@hillconsultingct.com>
2023-07-19 08:15:09 -0700

    add license file.

--- Data_Retrieval/Retrieval/encoding/New_Constant_Result.py
+++ Data_Retrieval/Retrieval/encoding/New_Constant_Result.py
@@ -1,3 +1,18 @@
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA  02110-1301, USA.
+
 COLUMN_NAME = ['Is_Dead/blank_0',
 'Death_date/Age_at_death_0_0',
 'recurrence_type/blank_0',
